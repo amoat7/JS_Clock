1f35975ed5aa5c3dd65fb3682f15aad6c1364d3e
amoat7 <amoat7@gmail.com>
2021-06-27 00:05:49 +1000

    set hour

---
+++ script.js
@@ -0,0 +1,87 @@
+const change = document.querySelector('.change'); 
+const root = document.querySelector(':root')
+const second = document.querySelector('.second');
+const hour = document.querySelector('.hour');
+
+const minute = document.querySelector('.minute');
+
+props = [
+    {
+        background_image: 'url(backgrounds/joker.jpeg)',
+        font_colour: 'white',
+        change_focus_colour : 'rgb(1, 7, 7)'
+    }, 
+    
+    {
+        background_image: 'url(backgrounds/map.jpeg)',
+        font_colour: 'white',
+        change_focus_colour : 'rgb(1, 7, 7)'
+    }, 
+
+    {
+        background_image: 'url(backgrounds/nature.jpeg)',
+        font_colour: 'white',
+        change_focus_colour : 'rgb(1, 7, 7)'
+    }, 
+
+    {
+        background_image: 'url(backgrounds/tile_green.jpeg)',
+        font_colour: 'white',
+        change_focus_colour : 'rgb(1, 7, 7)'
+    }, 
+
+    {
+        background_image: 'url(backgrounds/wood_brown.jpeg)',
+        font_colour: 'white',
+        change_focus_colour : 'rgb(1, 7, 7)'
+    }, 
+
+    {
+        background_image: 'url(backgrounds/brick_coloured.jpeg)',
+        font_colour: 'white',
+        change_focus_colour : 'rgb(1, 7, 7)'
+    }
+]
+
+function getRandomArbitrary(){
+    return Math.floor(Math.random() *  props.length);
+}
+
+
+function changeBackground(e){
+    selected = getRandomArbitrary();
+    root.style.setProperty(
+      "--background_image",
+      props[selected]["background_image"]
+    );
+    root.style.setProperty(
+        "--font_colour",
+        props[selected]["font_colour"]
+      );
+
+      root.style.setProperty(
+        "--change_focus_colour",
+        props[selected]["change_focus_colour"]
+      );
+    
+}
+change.addEventListener('click', changeBackground)
+function setDate(){
+    const now  = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = (seconds/60)*360;
+    second.style.transform =  `rotate(${secondsDegrees}deg)`
+
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = (minutes/60.0)*360;
+    minute.style.transform =  `rotate(${minutesDegrees+secondsDegrees/60}deg)`
+
+
+    const hours = now.getHours();
+    const hourDegrees = (hours/12)*360;
+    hour.style.transform =  `rotate(${hourDegrees+minutesDegrees/60}deg)`;
+
+    
+}
+setInterval(setDate, 1000);
